74dd2883903052dd1123741906f1ed4e1767f136
Ronaiza Cardoso <ronaiza.cardoso@co.pdmfc.com>
2019-08-18 12:16:12 +0100

    feat: add header configs

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
+import React from 'react'
+import { Image } from 'react-native'
 import { createAppContainer, createStackNavigator } from 'react-navigation'
+
+import logo from './assets/instagram.png'
 
 import Feed from './pages/Feed'
 
 const Routes = createAppContainer(
     createStackNavigator({
         Feed
+    }, {
+        headerLayoutPreset: 'center',
+        defaultNavigationOptions: {
+            headerTitle: <Image source={logo}/>,
+            headerStyle: {
+                backgroundColor: '#f5f5f5'
+            }
+        }
     })
 )
 
