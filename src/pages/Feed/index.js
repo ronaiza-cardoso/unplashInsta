1b801448d0a7cdbe86b9459c8206cf4f009d9054
Ronaiza Cardoso <ronaiza.cardoso@co.pdmfc.com>
2019-08-18 13:24:40 +0100

    feat: add feed list

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,67 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, FlatList } from 'react-native'
 
-// import { Container } from './styles';
+import { Post, Header, Avatar, Name, PostImage, PostDescription, Loading } from './style'
 
 export default function Feed () {
-  return <View />
+    const [feed, setFeed] = useState([])
+    const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [refreshing, setReshing] = useState(false)
+
+    async function loadPage(pageNumber = page, shouldRefresh = false) {
+        if(total && pageNumber > total) return
+        
+        setLoading(true)
+
+        const response = await fetch(
+            'http://localhost:3000/feed?_expand=author&_limit=5&_page=' + pageNumber
+        )
+        const data = await response.json()
+        const totalItems = response.headers.get('X-Total-Count')
+
+        setTotal(Math.floor(totalItems/5))
+        setFeed(shouldRefresh ? data : [...feed, ...data])
+        setPage(pageNumber + 1)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loadPage()
+    }, [])
+
+    async function refreshList () {
+        setReshing(true)
+
+        await loadPage(1, true)
+
+        setReshing(false)
+    }
+
+    return (
+        <View>
+            <FlatList 
+                data={feed}
+                keyExtractor={post => String(post.id)}
+                onEndReached={() => loadPage()}
+                onEndReachedThreshold={0.1}
+                onRefresh={refreshList}
+                refreshing={refreshing}
+                ListFooterComponent={loading && <Loading/>}
+                renderItem={({ item }) => (
+                    <Post>
+                        <Header>
+                            <Avatar ratio={item.aspectRatio} source={{ uri: item.author.avatar }}/>
+                            <Name>{item.author.name}</Name>
+                        </Header>
+                        <PostImage ratio={item.aspectRatio} source={{ uri: item.image }} />
+                        <PostDescription>
+                            <Name>{item.author.name}</Name> {item.description}
+                        </PostDescription>
+                    </Post>
+                )}
+            />
+        </View>
+    )
 }
---
+++ src/pages/Feed/style.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components/native'
+
+export const Post = styled.View`
+    margin-top: 15px;
+`
+
+export const Header = styled.View`
+    padding: 15px;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Avatar = styled.Image`
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+    margin-right: 10px;
+`
+
+export const Name = styled.Text`
+    color: #333;
+    font-weight: bold;
+`
+
+export const PostImage = styled.Image`
+    width: 100%;
+    aspect-ratio: ${props => props.ratio};
+`
+export const PostDescription = styled.Text`
+    padding: 15px;
+    line-height: 18px;
+`
+export const Loading = styled.ActivityIndicator.attrs({
+    size: 'small',
+    color: '#999'
+})`
+    margin: 30px 0;
+
+`
